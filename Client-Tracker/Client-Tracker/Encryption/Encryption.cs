273229bc3c0f435a4116c26831951a881afdf16d
Andy <andy@chiefmarley.com>
2010-07-29 18:11:44 -0500

    added encryption class

---
+++ Client-Tracker/Client-Tracker/Encryption/Encryption.cs
@@ -0,0 +1,399 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Client_Tracker.Encryption
+{
+    class Encryption
+    {
+        /// <summary>
+        /// Calculates SHA1 hash
+        /// </summary>
+        /// <param name="text">input string</param>
+        /// <param name="enc">Character encoding</param>
+        /// <returns>SHA1 hash</returns>
+        public static string CalculateSHA1(string text, Encoding enc)
+        {
+            byte[] buffer = enc.GetBytes(text);
+            SHA1CryptoServiceProvider cryptoTransformSHA1 =
+            new SHA1CryptoServiceProvider();
+            string hash = BitConverter.ToString(
+                cryptoTransformSHA1.ComputeHash(buffer)).Replace("-", "");
+
+            return hash;
+        }
+
+
+        public static string EncryptString(string inputString, int dwKeySize,
+                             string xmlString)
+        {
+            // TODO: Add Proper Exception Handlers
+            RSACryptoServiceProvider rsaCryptoServiceProvider =
+                                          new RSACryptoServiceProvider(dwKeySize);
+            rsaCryptoServiceProvider.FromXmlString(xmlString);
+            int keySize = dwKeySize / 8;
+            byte[] bytes = Encoding.UTF32.GetBytes(inputString);
+            // The hash function in use by the .NET RSACryptoServiceProvider here 
+            // is SHA1
+            // int maxLength = ( keySize ) - 2 - 
+            //              ( 2 * SHA1.Create().ComputeHash( rawBytes ).Length );
+            int maxLength = keySize - 42;
+            int dataLength = bytes.Length;
+            int iterations = dataLength / maxLength;
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i <= iterations; i++)
+            {
+                byte[] tempBytes = new byte[
+                        (dataLength - maxLength * i > maxLength) ? maxLength :
+                                                      dataLength - maxLength * i];
+                Buffer.BlockCopy(bytes, maxLength * i, tempBytes, 0,
+                                  tempBytes.Length);
+                byte[] encryptedBytes = rsaCryptoServiceProvider.Encrypt(tempBytes,
+                                                                          true);
+                // Be aware the RSACryptoServiceProvider reverses the order of 
+                // encrypted bytes. It does this after encryption and before 
+                // decryption. If you do not require compatibility with Microsoft 
+                // Cryptographic API (CAPI) and/or other vendors. Comment out the 
+                // next line and the corresponding one in the DecryptString function.
+                Array.Reverse(encryptedBytes);
+                // Why convert to base 64?
+                // Because it is the largest power-of-two base printable using only 
+                // ASCII characters
+                stringBuilder.Append(Convert.ToBase64String(encryptedBytes));
+            }
+            return stringBuilder.ToString();
+        }
+
+        public static string DecryptString(string inputString, int dwKeySize,
+                                     string xmlString)
+        {
+            // TODO: Add Proper Exception Handlers
+            RSACryptoServiceProvider rsaCryptoServiceProvider
+                                     = new RSACryptoServiceProvider(dwKeySize);
+            rsaCryptoServiceProvider.FromXmlString(xmlString);
+            int base64BlockSize = ((dwKeySize / 8) % 3 != 0) ?
+              (((dwKeySize / 8) / 3) * 4) + 4 : ((dwKeySize / 8) / 3) * 4;
+            int iterations = inputString.Length / base64BlockSize;
+            ArrayList arrayList = new ArrayList();
+            for (int i = 0; i < iterations; i++)
+            {
+                byte[] encryptedBytes = Convert.FromBase64String(
+                     inputString.Substring(base64BlockSize * i, base64BlockSize));
+                // Be aware the RSACryptoServiceProvider reverses the order of 
+                // encrypted bytes after encryption and before decryption.
+                // If you do not require compatibility with Microsoft Cryptographic 
+                // API (CAPI) and/or other vendors.
+                // Comment out the next line and the corresponding one in the 
+                // EncryptString function.
+                Array.Reverse(encryptedBytes);
+                arrayList.AddRange(rsaCryptoServiceProvider.Decrypt(
+                                    encryptedBytes, true));
+            }
+            return Encoding.UTF32.GetString(arrayList.ToArray(
+                                      Type.GetType("System.Byte")) as byte[]);
+        }
+
+
+    }
+    ///////////////////////////////////////////////////////////////////////////////
+    // SAMPLE: Symmetric key encryption and decryption using Rijndael algorithm.
+    // 
+    // To run this sample, create a new Visual C# project using the Console
+    // Application template and replace the contents of the Class1.cs file with
+    // the code below.
+    //
+    // THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+    // EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+    // WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+    // 
+    // Copyright (C) 2002 Obviex(TM). All rights reserved.
+    // 
+
+
+    /// <summary>
+    /// This class uses a symmetric key algorithm (Rijndael/AES) to encrypt and 
+    /// decrypt data. As long as encryption and decryption routines use the same
+    /// parameters to generate the keys, the keys are guaranteed to be the same.
+    /// The class uses static functions with duplicate code to make it easier to
+    /// demonstrate encryption and decryption logic. In a real-life application, 
+    /// this may not be the most efficient way of handling encryption, so - as
+    /// soon as you feel comfortable with it - you may want to redesign this class.
+    /// </summary>
+    public class SymetricEncryption
+    {
+        /// <summary>
+        /// Encrypts specified plaintext using Rijndael symmetric key algorithm
+        /// and returns a base64-encoded result.
+        /// </summary>
+        /// <param name="plainText">
+        /// Plaintext value to be encrypted.
+        /// </param>
+        /// <param name="passPhrase">
+        /// Passphrase from which a pseudo-random password will be derived. The
+        /// derived password will be used to generate the encryption key.
+        /// Passphrase can be any string. In this example we assume that this
+        /// passphrase is an ASCII string.
+        /// </param>
+        /// <param name="saltValue">
+        /// Salt value used along with passphrase to generate password. Salt can
+        /// be any string. In this example we assume that salt is an ASCII string.
+        /// </param>
+        /// <param name="hashAlgorithm">
+        /// Hash algorithm used to generate password. Allowed values are: "MD5" and
+        /// "SHA1". SHA1 hashes are a bit slower, but more secure than MD5 hashes.
+        /// </param>
+        /// <param name="passwordIterations">
+        /// Number of iterations used to generate password. One or two iterations
+        /// should be enough.
+        /// </param>
+        /// <param name="initVector">
+        /// Initialization vector (or IV). This value is required to encrypt the
+        /// first block of plaintext data. For RijndaelManaged class IV must be 
+        /// exactly 16 ASCII characters long.
+        /// </param>
+        /// <param name="keySize">
+        /// Size of encryption key in bits. Allowed values are: 128, 192, and 256. 
+        /// Longer keys are more secure than shorter keys.
+        /// </param>
+        /// <returns>
+        /// Encrypted value formatted as a base64-encoded string.
+        /// </returns>
+        public static string Encrypt(string plainText,
+                                     string passPhrase,
+                                     string saltValue,
+                                     string hashAlgorithm,
+                                     int passwordIterations,
+                                     string initVector,
+                                     int keySize)
+        {
+            // Convert strings into byte arrays.
+            // Let us assume that strings only contain ASCII codes.
+            // If strings include Unicode characters, use Unicode, UTF7, or UTF8 
+            // encoding.
+            byte[] initVectorBytes = Encoding.ASCII.GetBytes(initVector);
+            byte[] saltValueBytes = Encoding.ASCII.GetBytes(saltValue);
+
+            // Convert our plaintext into a byte array.
+            // Let us assume that plaintext contains UTF8-encoded characters.
+            byte[] plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+
+            // First, we must create a password, from which the key will be derived.
+            // This password will be generated from the specified passphrase and 
+            // salt value. The password will be created using the specified hash 
+            // algorithm. Password creation can be done in several iterations.
+            PasswordDeriveBytes password = new PasswordDeriveBytes(
+                                                            passPhrase,
+                                                            saltValueBytes,
+                                                            hashAlgorithm,
+                                                            passwordIterations);
+
+            // Use the password to generate pseudo-random bytes for the encryption
+            // key. Specify the size of the key in bytes (instead of bits).
+            byte[] keyBytes = password.GetBytes(keySize / 8);
+
+            // Create uninitialized Rijndael encryption object.
+            RijndaelManaged symmetricKey = new RijndaelManaged();
+
+            // It is reasonable to set encryption mode to Cipher Block Chaining
+            // (CBC). Use default options for other symmetric key parameters.
+            symmetricKey.Mode = CipherMode.CBC;
+
+            // Generate encryptor from the existing key bytes and initialization 
+            // vector. Key size will be defined based on the number of the key 
+            // bytes.
+            ICryptoTransform encryptor = symmetricKey.CreateEncryptor(
+                                                             keyBytes,
+                                                             initVectorBytes);
+
+            // Define memory stream which will be used to hold encrypted data.
+            MemoryStream memoryStream = new MemoryStream();
+
+            // Define cryptographic stream (always use Write mode for encryption).
+            CryptoStream cryptoStream = new CryptoStream(memoryStream,
+                                                         encryptor,
+                                                         CryptoStreamMode.Write);
+            // Start encrypting.
+            cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+
+            // Finish encrypting.
+            cryptoStream.FlushFinalBlock();
+
+            // Convert our encrypted data from a memory stream into a byte array.
+            byte[] cipherTextBytes = memoryStream.ToArray();
+
+            // Close both streams.
+            memoryStream.Close();
+            cryptoStream.Close();
+
+            // Convert encrypted data into a base64-encoded string.
+            string cipherText = Convert.ToBase64String(cipherTextBytes);
+
+            // Return encrypted string.
+            return cipherText;
+        }
+
+        /// <summary>
+        /// Decrypts specified ciphertext using Rijndael symmetric key algorithm.
+        /// </summary>
+        /// <param name="cipherText">
+        /// Base64-formatted ciphertext value.
+        /// </param>
+        /// <param name="passPhrase">
+        /// Passphrase from which a pseudo-random password will be derived. The
+        /// derived password will be used to generate the encryption key.
+        /// Passphrase can be any string. In this example we assume that this
+        /// passphrase is an ASCII string.
+        /// </param>
+        /// <param name="saltValue">
+        /// Salt value used along with passphrase to generate password. Salt can
+        /// be any string. In this example we assume that salt is an ASCII string.
+        /// </param>
+        /// <param name="hashAlgorithm">
+        /// Hash algorithm used to generate password. Allowed values are: "MD5" and
+        /// "SHA1". SHA1 hashes are a bit slower, but more secure than MD5 hashes.
+        /// </param>
+        /// <param name="passwordIterations">
+        /// Number of iterations used to generate password. One or two iterations
+        /// should be enough.
+        /// </param>
+        /// <param name="initVector">
+        /// Initialization vector (or IV). This value is required to encrypt the
+        /// first block of plaintext data. For RijndaelManaged class IV must be
+        /// exactly 16 ASCII characters long.
+        /// </param>
+        /// <param name="keySize">
+        /// Size of encryption key in bits. Allowed values are: 128, 192, and 256.
+        /// Longer keys are more secure than shorter keys.
+        /// </param>
+        /// <returns>
+        /// Decrypted string value.
+        /// </returns>
+        /// <remarks>
+        /// Most of the logic in this function is similar to the Encrypt
+        /// logic. In order for decryption to work, all parameters of this function
+        /// - except cipherText value - must match the corresponding parameters of
+        /// the Encrypt function which was called to generate the
+        /// ciphertext.
+        /// </remarks>
+        public static string Decrypt(string cipherText,
+                                     string passPhrase,
+                                     string saltValue,
+                                     string hashAlgorithm,
+                                     int passwordIterations,
+                                     string initVector,
+                                     int keySize)
+        {
+            // Convert strings defining encryption key characteristics into byte
+            // arrays. Let us assume that strings only contain ASCII codes.
+            // If strings include Unicode characters, use Unicode, UTF7, or UTF8
+            // encoding.
+            byte[] initVectorBytes = Encoding.ASCII.GetBytes(initVector);
+            byte[] saltValueBytes = Encoding.ASCII.GetBytes(saltValue);
+
+            // Convert our ciphertext into a byte array.
+            byte[] cipherTextBytes = Convert.FromBase64String(cipherText);
+
+            // First, we must create a password, from which the key will be 
+            // derived. This password will be generated from the specified 
+            // passphrase and salt value. The password will be created using
+            // the specified hash algorithm. Password creation can be done in
+            // several iterations.
+            PasswordDeriveBytes password = new PasswordDeriveBytes(
+                                                            passPhrase,
+                                                            saltValueBytes,
+                                                            hashAlgorithm,
+                                                            passwordIterations);
+
+            // Use the password to generate pseudo-random bytes for the encryption
+            // key. Specify the size of the key in bytes (instead of bits).
+            byte[] keyBytes = password.GetBytes(keySize / 8);
+
+            // Create uninitialized Rijndael encryption object.
+            RijndaelManaged symmetricKey = new RijndaelManaged();
+
+            // It is reasonable to set encryption mode to Cipher Block Chaining
+            // (CBC). Use default options for other symmetric key parameters.
+            symmetricKey.Mode = CipherMode.CBC;
+
+            // Generate decryptor from the existing key bytes and initialization 
+            // vector. Key size will be defined based on the number of the key 
+            // bytes.
+            ICryptoTransform decryptor = symmetricKey.CreateDecryptor(
+                                                             keyBytes,
+                                                             initVectorBytes);
+
+            // Define memory stream which will be used to hold encrypted data.
+            MemoryStream memoryStream = new MemoryStream(cipherTextBytes);
+
+            // Define cryptographic stream (always use Read mode for encryption).
+            CryptoStream cryptoStream = new CryptoStream(memoryStream,
+                                                          decryptor,
+                                                          CryptoStreamMode.Read);
+
+            // Since at this point we don't know what the size of decrypted data
+            // will be, allocate the buffer long enough to hold ciphertext;
+            // plaintext is never longer than ciphertext.
+            byte[] plainTextBytes = new byte[cipherTextBytes.Length];
+
+            // Start decrypting.
+            int decryptedByteCount = cryptoStream.Read(plainTextBytes,
+                                                       0,
+                                                       plainTextBytes.Length);
+
+            // Close both streams.
+            memoryStream.Close();
+            cryptoStream.Close();
+
+            // Convert decrypted data into a string. 
+            // Let us assume that the original plaintext string was UTF8-encoded.
+            string plainText = Encoding.UTF8.GetString(plainTextBytes,
+                                                       0,
+                                                       decryptedByteCount);
+
+            // Return decrypted string.   
+            return plainText;
+        }
+    }
+
+    public static class DigitalSigning
+    {
+
+        public static string CreateSignature(string textToSign, string XMLprivateKey)
+        {
+            RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
+            RSA.FromXmlString(XMLprivateKey);
+            RSAPKCS1SignatureFormatter RSAFormatter = new RSAPKCS1SignatureFormatter(RSA);
+            RSAFormatter.SetHashAlgorithm("SHA1");
+            SHA1Managed SHhash = new SHA1Managed();
+            byte[] SignedHashValue = RSAFormatter.CreateSignature(SHhash.ComputeHash(new UnicodeEncoding().GetBytes(textToSign)));
+            string signature = System.Convert.ToBase64String(SignedHashValue);
+            return signature;
+
+        }
+
+        public static bool VerifySignature(string textToVerify, string signature, string XMLpublicKey)
+        {
+            RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
+            RSA.FromXmlString(XMLpublicKey);
+            RSAPKCS1SignatureDeformatter RSADeformatter = new RSAPKCS1SignatureDeformatter(RSA);
+            RSADeformatter.SetHashAlgorithm("SHA1");
+            SHA1Managed SHhashpub = new SHA1Managed();
+            if (RSADeformatter.VerifySignature(
+                 SHhashpub.ComputeHash(new UnicodeEncoding().GetBytes(textToVerify)),
+                 System.Convert.FromBase64String(signature))
+                 )
+            {
+                /// The signature is valid.
+                return true;
+            }
+            else
+            {
+                /// The signature is not valid.
+                return false;
+            }
+        }
+
+    }
+}
